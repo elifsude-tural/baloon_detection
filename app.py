dc5f205769a9af69415dc7a4ba41f61e2a0bfe93
elifsudetural <elifsudeturall@gmail.com>
2026-03-10 18:46:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -57,9 +57,46 @@ def draw_detection(frame, x1, y1, x2, y2, label, color, conf):
                 0.7, (255, 255, 255), 2)
 
 
+"""def draw_crosshair(frame):
+    h, w = frame.shape[:2]
+    cx, cy = w // 2, h // 2
+    color = (0, 255, 0)
+    size, gap, thick = 30, 8, 2
+
+    cv2.line(frame, (cx - size, cy), (cx - gap, cy), color, thick)
+    cv2.line(frame, (cx + gap, cy), (cx + size, cy), color, thick)
+    cv2.line(frame, (cx, cy - size), (cx, cy - gap), color, thick)
+    cv2.line(frame, (cx, cy + gap), (cx, cy + size), color, thick)
+    cv2.circle(frame, (cx, cy), 2, color, -1)
+"""
+crosshair_img = cv2.imread("crosshair2.png", cv2.IMREAD_UNCHANGED)  # UNCHANGED şeffaflık için
+
+
+
+def draw_crosshair(frame, size=200):
+    h, w = frame.shape[:2]
+    cx, cy = w // 2, h // 2
+
+    # Nişangahı istediğin boyuta getir
+    ch = cv2.resize(crosshair_img, (size, size))
+
+    # Yapıştırılacak bölge
+    x1 = cx - size // 2
+    y1 = cy - size // 2
+    x2 = x1 + size
+    y2 = y1 + size
+
+    # Alpha (şeffaflık) kanalını ayır
+    alpha = ch[:, :, 3] / 255.0
+    for c in range(3):
+        frame[y1:y2, x1:x2, c] = (
+            alpha * ch[:, :, c] +
+            (1 - alpha) * frame[y1:y2, x1:x2, c]
+        )
 
 def main():
-    cap = cv2.VideoCapture(0)
+    #cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)  # Windows için
     if not cap.isOpened():
         print("Camera didn't Open.")
         return
@@ -86,20 +123,19 @@ def main():
             elif label =="Friendly": friendly_count += 1
             else: unknown_count +=1
 
-            
-
-        
+        draw_crosshair(frame)
         cv2.putText(frame, f"Enemy:    {enemy_count}",    (10, 30),  cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255),   2)
         cv2.putText(frame, f"Friendly: {friendly_count}", (10, 60),  cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0),   2)
         cv2.putText(frame, f"Unknown:  {unknown_count}",  (10, 90),  cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0),   2)
         
-
         cv2.imshow("Baloon detection", frame)
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
 
     cap.release()
     cv2.destroyAllWindows()
+
+    print(cap.get(cv2.CAP_PROP_FRAME_WIDTH), cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
 if __name__ == "__main__":
     main()
